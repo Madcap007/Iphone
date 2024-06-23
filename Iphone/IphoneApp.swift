1e5826c960c78b05fd2e71e4d85cd1afda5b877e
Madcap007 <shovonmdabusalek@gmail.com>
2024-06-22 22:24:18 -0700

    Initial Commit

---
+++ Iphone/IphoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  IphoneApp.swift
+//  Iphone
+//
+//  Created by MD ABUSALEK SHOVON on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct IphoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
